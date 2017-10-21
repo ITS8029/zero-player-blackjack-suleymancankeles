344af0fea965d222daff8f00ab90f3ba27ca20c2
suleymancankeles <32978543+suleymancankeles@users.noreply.github.com>
2017-10-21 15:02:50 -0400

    Update ZeroPlayerBlackjack.java

--- ZeroPlayerBlackjack.java
+++ ZeroPlayerBlackjack.java
@@ -1,9 +1,29 @@
-package com.adampolt;
+
 
 public class ZeroPlayerBlackjack {
 
 	public static void main(String[] args) {
-		//Your code here.
+	
+		Deck deck = new Deck();
+		
+		int total = 0;
+		
+		while(total < 17) {
+		Card card = deck.drawCard();
+		
+		System.out.println("Your card is " + card.toString());
+		
+		total += card.getValue();
 	}
-
-}}
+	
+	System.out.println("Your total is " + total);
+	
+	if(total == 21) {
+		System.out.println("Blackjack!");
+	} else if (total < 21) {
+	System.out.println("You win!");
+	} else {
+		System.out.println("You lose!");
+	}
+	
+}}	
